79e8463c283d53a83d9cd5e956dc2cb63ca857fa
unknown <pietari.m@hotmail.com>
2021-04-26 15:40:19 +0300

    pitäisi toimia https

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 /* import express from 'express'; */
-require('dotenv').config()
+/* require('dotenv').config() */
 const express = require('express')
 /* import Tehtava from './schema/tehtava' */
 const jwt = require('jsonwebtoken')
